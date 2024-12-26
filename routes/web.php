7559a2121d6801b1f0675591213c58fcebc754df
tomasgeneroso <tomasgeneroso90@gmail.com>
2024-12-26 19:30:46 +0100

    creacion de rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\API\Controller_Products;
 Route::get('/', function () {
     return view('welcome');
+});
+
+//doesn't work with ruta/api/getproducts so i just use web.php por the routes
+//!Intentar poner luego el prefix
+Route::controller(Controller_Products::class)->group(function () { 
+    Route::get('/getproducts','getProducts');
+    Route::post("/createproduct", "createProduct");
+    Route::put("/updateproduct", "updateProduct");
+    Route::delete("/deleteproduct",  "deleteProduct");
 });
